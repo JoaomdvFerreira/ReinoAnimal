6b38f4ee55e3d0aedfc830b219169acfc4343b9b
Joao Ferreira <joao.mdvferreira@gmail.com>
2020-10-17 21:02:29 +0100

    added styled components + navbar completed + Home Page

---
+++ reino-animal/src/components/InfoSection/InfoSection.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Container } from '../../globalStyles';
+import { InfoSec, InfoRow, InfoColumn, TextWrapper, TopLine, Heading, SubTitle, ImgWrapper, Img } from './InfoSection.elements';
+
+const InfoSection = ({
+    lightBg,
+    imgStart,
+    lightTopLine,
+    lightText,
+    lightTextDesc,
+    primary,
+    buttonLabel,
+    description,
+    headline,
+    topLine,
+    start,
+    img,
+    alt }) => {
+    return (
+        <>
+            <InfoSec lightBg={lightBg}>
+                <Container>
+                    <InfoRow imgStart={imgStart}>
+                        <InfoColumn>
+                            <TextWrapper>
+                                <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                                <Heading lightText={lightText}>{headline}</Heading>
+                                <SubTitle lightTextDesc={lightTextDesc}>{description}</SubTitle>
+                                <Link to='/sign-up'>
+                                    <Button big fontBig primary={primary}>{buttonLabel}</Button>
+                                </Link>
+                            </TextWrapper>
+                        </InfoColumn>
+                        <InfoColumn>
+                            <ImgWrapper start={start}>
+                                <Img src={img} alt={alt} />
+                            </ImgWrapper>
+                        </InfoColumn>
+                    </InfoRow>
+                </Container>
+            </InfoSec>
+        </>
+    )
+}
+
+export default InfoSection
--- reino-animal/src/components/navbar/Navbar.js
+++ reino-animal/src/components/navbar/Navbar.js
@@ -1,39 +1,74 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React, { useState, useEffect } from 'react';
+import { CgMenuRight } from 'react-icons/cg';
+import { AiOutlineClose } from 'react-icons/ai';
+import { IconContext } from 'react-icons/lib';
+import { Button } from '../../globalStyles';
+import {
+    Nav,
+    NavBarContainer,
+    NavLogo,
+    NavIcon,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavItemBtn,
+    NavBtnLink
+} from './Navbar.elements';
 
-function Navbar() {
+const Navbar = () => {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
+    const showButton = () => window.innerWidth <= 960 ? setButton(false) : setButton(true);
     const closeMobileMenu = () => setClick(false);
+
+    useEffect(() => {
+        showButton()
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
     return (
         <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">Reino Animal</Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                    </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>Empresa</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>Reino Animal</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>Contactos</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>Loja Online</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <Nav>
+                    <NavBarContainer >
+                        <NavLogo to="/" onClick={closeMobileMenu}>
+                            <NavIcon />
+                        </NavLogo>
+                        <MobileIcon onClick={handleClick}>
+                            {click ? <AiOutlineClose /> : <CgMenuRight />}
+                        </MobileIcon>
+                        <NavMenu onClick={handleClick} click={click}>
+                            <NavItem>
+                                <NavLinks to="/">Home</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to="/services">Services</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to="/products">Products</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to="/contacts">Contacts</NavLinks>
+                            </NavItem>
+                            <NavItemBtn>
+                                {button ? (
+                                    <NavBtnLink to='/sign-up'>
+                                        <Button primary>SIGN-UP</Button>
+                                    </NavBtnLink>
+                                ) : (
+                                        <NavBtnLink to="/sign-up">
+                                            <Button primary>SIGN-UP</Button>
+                                        </NavBtnLink>
+                                    )}
+                            </NavItemBtn>
+                        </NavMenu>
+                    </NavBarContainer>
+                </Nav>
+            </IconContext.Provider>
         </>
     )
 }
